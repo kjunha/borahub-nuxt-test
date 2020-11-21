4dc55116c7a45b7e40c696aad39282fd01a75826
kjunha <kjunha77@gmail.com>
2020-11-22 00:06:20 +0900

    add picture layout markup

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="landing">
     <div class="main" :style="mainStyle">
-      <transition name="fade">
-        <b-input-group>
-          <template #prepend>
-            <b-dropdown text="Dropdown" variant="secondary">
-              <b-dropdown-item>Action A</b-dropdown-item>
-              <b-dropdown-item>Action B</b-dropdown-item>
-            </b-dropdown>
-          </template>
-          <b-form-input></b-form-input>
-          <template #append>
-            <b-button variant="success">Button</b-button>
-          </template>
-        </b-input-group>
-      </transition>
+      <b-input-group>
+        <template #prepend>
+          <b-dropdown text="Dropdown" variant="secondary">
+            <b-dropdown-item>Action A</b-dropdown-item>
+            <b-dropdown-item>Action B</b-dropdown-item>
+          </b-dropdown>
+        </template>
+        <b-form-input></b-form-input>
+        <template #append>
+          <b-button variant="success">Button</b-button>
+        </template>
+      </b-input-group>
     </div>
     <div v-if="!galleryToggle" class="text-center pt-5">
       <div class="main-scroll-1">Scroll Down</div>
@@ -23,7 +21,11 @@
       <div class="main-scroll-4">˅</div>
     </div>
     <div v-else class="gallery">
-
+      <transition-group name="slide-fade" class="frame-wall d-flex justify-content-center flex-wrap">
+        <div v-show="frameToggle" class="frame" v-for="i in 33" :key="i">
+          <img src="~/assets/sample.jpg" alt="title">
+        </div>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -33,6 +35,7 @@ export default {
   data() {
     return {
       galleryToggle: false,
+      frameToggle: false,
       mainStyle: {
         marginTop: '45vh',
         position: 'static'
@@ -43,6 +46,7 @@ export default {
     detectWindowScroll() {
       if(window.scrollY < 1) {
         this.galleryToggle = false;
+        this.frameToggle = false;
         this.mainStyle = {
           marginTop: '45vh',
           position: 'static',
@@ -54,11 +58,13 @@ export default {
           top: 0,
           paddingTop: '7px',
           paddingBottom: '7px',
+          backgroundColor: '#eee',
           boxShadow: '1px 3px 6px 0 #ccc'
         }
+        setTimeout(() => {this.frameToggle = true;}, 300)
         console.log('is called')
       }
-    }
+    },
   },
   mounted() {
     window.addEventListener('scroll', this.detectWindowScroll)
@@ -112,9 +118,27 @@ export default {
   0%{color: #999;}
   100%{color: #ccc;}
 }
+.slide-fade-enter-active, .slide-fade-leave-active {
+  transition: all .8s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
+}
 
 .landing{
   height: 105vh;
+}
+
+.gallery {
+  padding-top: 12vh;
+}
+.frame {
+  margin: 2.7px;
+}
+.frame img{
+  width: 365px;
+  height: 365px;
 }
 
 </style>
